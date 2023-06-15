2c7672a2f6aa4bb3d6250250f4f74c318fe9af14
cha1rsama <120267442+cha1rsama@users.noreply.github.com>
2023-06-15 17:30:23 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TELEGRAM_BOT_TOKEN = "5563989537:AAH8UXjQe-RVFaaUoKJgqf3qCejyxPpXw4g"
+TELEGRAM_BOT_TOKEN = "5875559391:AAFgAKGbr4jAZOpeVuJwE-TNykrh3H3zGp0"
 WEBHOOK_URL = "https://fastapi-production-4f92.up.railway.app"
 
