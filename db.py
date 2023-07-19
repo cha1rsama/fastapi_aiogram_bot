5bbcbed096c6325c174644412a528cc930cee845
cha1rsama <120267442+cha1rsama@users.noreply.github.com>
2023-07-19 17:26:52 +0500

    Create db.py

---
+++ db.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+cluster = MongoClient(
+    'mongodb+srv://gogikakbarov99:6mwcOy4SfqYfE7kd@cluster0.66u3tri.mongodb.net/?retryWrites=true&w=majority')
+db = cluster['Oneapp_db']
+collection = db['user_id_chat_id']
+
+# user_token = {}
+database = {}
+
+
+async def add_user(chat_id, username, lang_code):
+    collection.insert_one({
+        '_id': chat_id,
+        'username': username,
+        'lang_code': lang_code
+    })
+
+
+async def update_user(chat_id, token):
+    filters = {'_id': chat_id}
+    collection.update_one(filters, {"$set": {'token': token}})
+    # user_token.update([(f'{chat_id}', f'{token}')])
+
+
+async def get_token(chat_id):
+    filters = {'_id': chat_id}
+    a = collection.find_one(filters)
+    return a
