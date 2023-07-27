90689709525f3778b9d890e0f69e607b7da1d549
cha1rsama <120267442+cha1rsama@users.noreply.github.com>
2023-07-27 23:23:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import types, Dispatcher, Bot
 from bot import dp, bot
 from config import  WEBHOOK_URL
 import uvicorn
-
+import os
 app = FastAPI()
 token = os.environ.get("TELEGRAM_TOKEN")
 WEBHOOK_PATH = f"/bot/{token}"
