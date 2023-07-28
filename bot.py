bfa4858d87f4678dc07c9f8eaaf43ff0d8c1f51a
cha1rsama <120267442+cha1rsama@users.noreply.github.com>
2023-07-28 15:23:37 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,6 +80,7 @@ async def prev_page_handler(callback: types.CallbackQuery):
 @dp.message_handler(content_types=['web_app_data'])
 async def web_app(message: types.Message):
     data = message.web_app_data
+    print (data)
     token = data.data.strip("\"")
     await main_page(token, message)
 
